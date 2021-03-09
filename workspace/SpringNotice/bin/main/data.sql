2b32876fcff8db7d1558c8d7e1ff6fba3298ae13
beomk5414 <the3011@gmail.com>
2021-03-09 16:41:48 +0900

    add file

---
+++ workspace/SpringNotice/bin/main/data.sql
@@ -0,0 +1,47 @@
+-- MEMBER
+INSERT 
+  INTO MEMBER
+       (ID, PW, NAME, GENDER, EMAIL, ROLE, REGDATE)
+VALUES ('admin', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', '관리자', 'Male', 'admin@rsupport.com', 'ADMIN', CURRENT_TIMESTAMP);	
+
+INSERT 
+  INTO MEMBER
+       (ID, PW, NAME, GENDER, EMAIL, ROLE, REGDATE)
+VALUES ('user1', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', '사용자1', 'Male', 'user1@rsupport.com', 'USER', CURRENT_TIMESTAMP);	
+
+INSERT 
+  INTO MEMBER
+       (ID, PW, NAME, GENDER, EMAIL, ROLE, REGDATE)
+VALUES ('user2', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', '사용자2', 'Male', 'user2@rsupport.com', 'USER', CURRENT_TIMESTAMP);	
+
+-- NOTICE
+INSERT
+  INTO NOTICE
+       (WRITER, TITLE, CONTENT, FILENAME, REGDATE, LASTEDITED)
+VALUES ('admin', '공지사항 입니다.', '공지사항 테스트 입니다.', '', '2021-03-02', '2021-03-02');
+INSERT
+  INTO NOTICE
+       (WRITER, TITLE, CONTENT, FILENAME, REGDATE, LASTEDITED)
+VALUES ('admin', '공지사항 입니다.', '공지사항 테스트 입니다.', '', '2021-03-03', '2021-03-03');
+INSERT
+  INTO NOTICE
+       (WRITER, TITLE, CONTENT, FILENAME, REGDATE, LASTEDITED)
+VALUES ('admin', '공지사항 입니다.', '공지사항 테스트 입니다.', '', '2021-03-04', '2021-03-04');
+INSERT
+  INTO NOTICE
+       (WRITER, TITLE, CONTENT, FILENAME, REGDATE, LASTEDITED)
+VALUES ('admin', '공지사항 입니다.', '공지사항 테스트 입니다.', '', '2021-03-05', '2021-03-05');
+INSERT
+  INTO NOTICE
+       (WRITER, TITLE, CONTENT, FILENAME, REGDATE, LASTEDITED)
+VALUES ('admin', '공지사항 입니다.', '공지사항 테스트 입니다.', 'example3_1615269758076.jpg&', '2021-03-06', '2021-03-06');
+INSERT
+  INTO NOTICE
+       (WRITER, TITLE, CONTENT, FILENAME, REGDATE, LASTEDITED)
+VALUES ('admin', '공지사항 입니다.', '공지사항 테스트 입니다.', 'example4_1615270201245.mp4&', '2021-03-07', '2021-03-07');
+INSERT
+  INTO NOTICE
+       (WRITER, TITLE, CONTENT, FILENAME, REGDATE, LASTEDITED)
+VALUES ('admin', '공지사항 입니다.', '공지사항 테스트 입니다.', 'example_1615269758074.jpg&example2_1615269758075.jpg&', '2021-03-08', '2021-03-08');
+
+
